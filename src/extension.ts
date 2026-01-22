8566c1ff51ae1f2a75c32a7ea2074e3d941996f5
Lucas Lannes <lucaslannesr@gmail.com>
2026-01-21 23:59:54 -0300

    Run workflows in extension activation

--- src/extension.ts
+++ src/extension.ts
@@ -1,46 +1,6 @@
-import * as fs from "fs"
-import * as path from "path"
 import * as vscode from "vscode"
-import { downloadYumlParser, runYumlParser } from "./yuml-parser"
-import { getYumlJson } from "./yuml-json"
-import { YumlImportCompletionProvider } from "./providers/yuml-import-completion-provider"
-import { YumlFunctionCompletionProvider } from "./providers/yuml-function-completion-provider"
+import { workflows } from "./workflows";
 
 export async function activate(context: vscode.ExtensionContext) {
-  const workspaceFolders = vscode.workspace.workspaceFolders
-	if (!workspaceFolders) return
-	
-	const root = workspaceFolders[0].uri.fsPath
-	const yumlJsonPath = path.join(root, "yuml.json")
-	const yumlDir = path.join(root, ".yuml")
-
-	if (fs.existsSync(yumlJsonPath)) {
-		if (!fs.existsSync(yumlDir)) {
-			fs.mkdirSync(yumlDir)
-		}
-
-		const yumlParser = await downloadYumlParser(root)
-		if (!yumlParser) return
-		
-		watchYumlFiles()
-
-		context.subscriptions.push(YumlImportCompletionProvider);
-		context.subscriptions.push(YumlFunctionCompletionProvider);
-	}
-}
-
-function watchYumlFiles() {
-  const workspaceFolders = vscode.workspace.workspaceFolders
-	if (!workspaceFolders) return
-	
-	const root = workspaceFolders[0].uri.fsPath
-	const yumlJson = getYumlJson()
-	if (!yumlJson) return
-
-	const workflows = path.join(root, yumlJson.workflows)
-	const watcher = vscode.workspace.createFileSystemWatcher(
-    new vscode.RelativePattern(workflows, "**/*.yuml")
-  )
-  watcher.onDidChange(uri => runYumlParser(uri.fsPath))
-  watcher.onDidCreate(uri => runYumlParser(uri.fsPath))
+	await workflows.run('extension', 'activate', { args: { context } })
 }
